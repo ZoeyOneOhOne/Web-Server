460e65f4c3af73b0bfb48fd81fe0303d85ad3a93
Cole Perry <perry391@marshall.edu>
2018-11-29 19:31:45 -0500

    Changes on Thursday

--- Web Server/src/WebServer.java
+++ Web Server/src/WebServer.java
@@ -10,11 +10,12 @@ public class WebServer {
 		int port = 8080;
 		
 		// Establish the listen socket.
-		 //???? One line of code goes here
+		DatagramSocket socket = new DatagramSocket(port);
 		// Process HTTP service requests in an infinite loop.
 		while (true) {
 		// Listen for a TCP connection request.
-		//???? One line of code goes here
+			DatagramPacket request = new DatagramPacket(new byte[1024], 1024);
+			socket.receive(request); // Block until the host receives a UDP packet.
 			
 			// Construct an object to process the HTTP request message.
 			HttpRequest request = new HttpRequest(?????);
@@ -26,6 +27,7 @@ public class WebServer {
 	
 	final class HttpRequest implements Runnable
 	{
+		
 	
 		final static String CRLF = "\r\n";
 		Socket socket;
@@ -37,11 +39,40 @@ public class WebServer {
 		// Implement the run() method of the Runnable interface.
 		public void run()
 		{
-		
+			try {
+				processRequest();
+				} catch (Exception e) {
+				System.out.println(e);
+				}
 		}
 		private void processRequest() throws Exception
 		{
-		
+			// Get a reference to the socket's input and output streams.
+			InputStream is = ????;
+			DataOutputStream os = ????;
+			// Set up input stream filters.
+			???? One line of code goes here
+			BufferedReader br = ????;
+			
+			// Get the request line of the HTTP request message.
+			String requestLine = ????;
+			// Display the request line.
+			System.out.println();
+			System.out.println(requestLine);
+			
+			// Get and display the header lines.
+			String headerLine = null;
+			while ((headerLine = br.readLine()).length() != 0) {
+			System.out.println(headerLine);
+			}
+			
+			(headerLine = br.readLine()).length();
+			
+			// Close streams and socket.
+			os.close();
+			br.close();
+			socket.close();
 		}
 	}
+  }
 }
